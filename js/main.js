482925d90a19b0925a2df46972d22e1457ecfcd9
M1caela <micaelaacalvo@gmail.com>
2025-02-22 21:13:04 -0300

    css del home/se agregan imagenes y links

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+    var parallaxImage = document.querySelector('.parallax-backg img');
+
+    /* Se mueve más lento que el scroll para el efecto parallax */
+    parallaxImage.style.transform = 'translateY(' + scrollPosition * 0.3 + 'px)';
+});
