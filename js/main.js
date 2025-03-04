26fa72066f18499a4f06e34fc4d30f45a5ffd05a
M1caela <micacalvoo@gmail.com>
2025-03-04 11:36:18 -0300

    aplicando estilos a 'gastronomia' - seccion de comidas tradicionales y banner

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+// efecto parallax
 window.addEventListener('scroll', function () {
     var scrollPosition = window.scrollY;
     var parallaxImage = document.querySelector('.parallax-backg img');
@@ -6,3 +7,32 @@ window.addEventListener('scroll', function () {
     /* Se mueve más lento que el scroll para el efecto parallax */
     parallaxImage.style.transform = 'translateY(' + scrollPosition * 0.3 + 'px)';
 });
+
+
+
+// Navbar
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+});
+
+document.getElementById("close-menu").addEventListener("click", function () {
+    document.getElementById("mobile-menu").classList.add("hidden");
+});
+
+function toggleDropdown(id) {
+    document.getElementById(id).classList.toggle("hidden");
+}
+
+let closeTimeout;
+
+function openDropdown(id) {
+    clearTimeout(closeTimeout); // Cancela el cierre si el usuario vuelve a hacer hover
+    document.getElementById(id).classList.remove("hidden");
+}
+
+function closeDropdown(id) {
+    closeTimeout = setTimeout(function () {
+        document.getElementById(id).classList.add("hidden");
+    }, 300); // Delay de 300ms antes de cerrar el menú
+}
+
